5e2aeb852f7580361a7b67d19169facfa985950b
Ivan Rodionov <144949516+akerumort@users.noreply.github.com>
2024-09-01 14:21:19 +0300

    added CustomValidationException

---
+++ src/main/java/com/akerumort/VacationPayCalculator/exceptions/CustomValidationException.java
@@ -0,0 +1,7 @@
+package com.akerumort.VacationPayCalculator.exceptions;
+
+public class CustomValidationException extends RuntimeException{
+    public CustomValidationException(String message) {
+        super(message);
+    }
+}
